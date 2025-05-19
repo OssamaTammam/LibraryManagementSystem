d9e3100aa838eca7dda6cc39889c584750e7489f
Ossama <ossamatammam@gmail.com>
2025-05-19 21:03:34 +0300

    Create transactions migration

---
+++ backend/db/migrate/20250519175541_create_transactions.rb
@@ -0,0 +1,15 @@
+class CreateTransactions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :transactions do |t|
+      t.bigint :user_id, null: false
+      t.bigint :book_id, null: false
+      t.decimal :price, null: false
+      t.integer :transaction_type, null: false
+      t.datetime :transaction_date, null: false
+      t.datetime :return_date, null: false
+    end
+
+    add_foreign_key :transactions, :users, column: :user_id
+    add_foreign_key :transactions, :books, column: :book_id
+  end
+end
