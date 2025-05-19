c58cad3435c94cab8c7e5c3dba4f16b7f346241d
Ossama <ossamatammam@gmail.com>
2025-05-19 21:13:16 +0300

    migations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_19_175541) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_181212) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.string "author", null: false
